4bdf389f75d1b1ff5d43ea9fd01604d060059cf8
nictrak <narin14175@gmail.com>
2020-05-08 18:29:09 +0700

    board render complete

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { PureComponent } from "react";
 import { GameEngine } from "react-game-engine";
-import Tile from "./entities/tile";
-import AdSense from 'react-adsense';
+import Board from "./entities/board"
 export default class App extends PureComponent {
   render() {
     return (
-      <GameEngine
-        style={{ width: 1280, height: 720, backgroundColor: "#BEE7B8", margin:"auto"}}
-        systems={[]}
-        entities={{
-          //-- Notice that each entity has a unique id (required)
-          //-- and a renderer property (optional). If no renderer
-          //-- is supplied with the entity - it won't get displayed.
-          tile1: { x: 200,  y: 200, renderer: <Tile />}
-        }}>
-      </GameEngine>
+      <div style={{position: "absolute", left: "50%"}}>
+        <GameEngine
+          style={{ width: 1280, height: 720, backgroundColor: "#BEE7B8", position: "relative", left: "-50%"}}
+          systems={[]}
+          entities={{
+            //-- Notice that each entity has a unique id (required)
+            //-- and a renderer property (optional). If no renderer
+            //-- is supplied with the entity - it won't get displayed.
+            board: { position: {x: 640, y: 300},renderer: <Board />}
+          }}>
+        </GameEngine>
+      </div>
       
     );
   }
---
+++ src/entities/board.js
@@ -0,0 +1,66 @@
+import React from "react"
+import { render } from "@testing-library/react"
+
+
+const index2D = (indexX, indexY) => ({x: indexX, y: indexY})
+
+const Tile = (props) => {
+    const size = 64;
+    const x = props.position.x - size / 2;
+    const y = props.position.y - size / 2;
+    return (
+        <img src={require('../sprite/tile.png')} style={{ position: "absolute", width: size, height: size, left: x, top: y}}/>
+    )
+}
+
+const gridToAbsolute = (gridPostion, boardPosition) => ({
+    x: (gridPostion.x-3.5)*68 + boardPosition.x,
+    y: (gridPostion.y-3.5)*68 + boardPosition.y
+})
+
+const renderTile = (indexX, indexY, boardPosition) => <Tile position={gridToAbsolute(index2D(indexX, indexY),boardPosition)}/>
+
+const renderRow = (indexY, boardPosition) => {
+    return (
+        <div>
+            {renderTile(0, indexY, boardPosition)}
+            {renderTile(1, indexY, boardPosition)}
+            {renderTile(2, indexY, boardPosition)}
+            {renderTile(3, indexY, boardPosition)}
+            {renderTile(4, indexY, boardPosition)}
+            {renderTile(5, indexY, boardPosition)}
+            {renderTile(6, indexY, boardPosition)}
+            {renderTile(7, indexY, boardPosition)}
+        </div>
+    )
+}
+
+const renderBoard = (boardPosition) => {
+    return (
+        <div>
+            {renderRow(0, boardPosition)}
+            {renderRow(1, boardPosition)}
+            {renderRow(2, boardPosition)}
+            {renderRow(3, boardPosition)}
+            {renderRow(4, boardPosition)}
+            {renderRow(5, boardPosition)}
+            {renderRow(6, boardPosition)}
+            {renderRow(7, boardPosition)}
+        </div>
+    )
+}
+
+
+const Board = (props) => {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7]
+    const position = props.position
+    const indexX = 0
+    const indexY = 0
+    return(
+        <div>
+            {renderBoard(position)}
+        </div>
+    )
+}
+
+export default Board
